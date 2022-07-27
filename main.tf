d9c099ce339f950982ecbd63c9dd0ea34d4a345a
Shrishti943 <72365061+Shrishti943@users.noreply.github.com>
2022-07-27 15:18:24 +0530

    terraform

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ resource "azurerm_resource_group" "rg_gipush" {
   count               = length(var.rg_names)
   name                = var.rg_names[count.index]
   location            = var.rg_names[count.index] == "rg-az-jaea-prd-001" ? "Japan East" :  "SouthEast Asia"
+  tags = {
+    environment = "Production"
+  }
 }
