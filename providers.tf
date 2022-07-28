9edc68c7923f363898ce0f45b0a3496431ad0686
Shrishti943 <72365061+Shrishti943@users.noreply.github.com>
2022-07-28 13:46:18 +0530

    terraform

--- providers.tf
+++ providers.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
 }
-
 provider "azurerm" {
   # Configuration options
+  tenant_id = "d70cb4db-69c4-4a49-aaf3-f1e7ac277686"
+  subscription_id = "67669368-3d09-43a4-ae32-86cee13df434"
   features {
-    
   }
 }
