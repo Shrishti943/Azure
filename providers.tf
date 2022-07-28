e42f46ebce281b51fd6a2ae25c1e6bf335c08cf4
Shrishti943 <72365061+Shrishti943@users.noreply.github.com>
2022-07-28 18:16:58 +0530

    terraform

--- providers.tf
+++ providers.tf
@@ -8,6 +8,8 @@ terraform {
 }
 provider "azurerm" {
   # Configuration options
+  client_id = "ec9341f0-096a-4075-a187-6c7a468974b1"
+  client_secret = "JRW8Q~WjWDcrphld2WOSLC5iIMT31XylG49rQa-O"
   tenant_id = "d70cb4db-69c4-4a49-aaf3-f1e7ac277686"
   subscription_id = "67669368-3d09-43a4-ae32-86cee13df434"
   features {
